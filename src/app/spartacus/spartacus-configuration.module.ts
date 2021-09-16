a7f6a929fe6d76ecc008e2bae37da516d1967f3d
Gilberto Alvarado <gilberto.alvarado.jimenez@sap.com>
2021-09-16 17:42:05 -0400

    Adding PWA

--- src/app/spartacus/spartacus-configuration.module.ts
+++ src/app/spartacus/spartacus-configuration.module.ts
@@ -1,35 +1,52 @@
 import { NgModule } from '@angular/core';
-import { translationChunksConfig, translations } from "@spartacus/assets";
-import { FeaturesConfig, I18nConfig, OccConfig, provideConfig, SiteContextConfig } from "@spartacus/core";
-import { defaultCmsContentProviders, layoutConfig, mediaConfig } from "@spartacus/storefront";
+import { translationChunksConfig, translations } from '@spartacus/assets';
+import {
+  FeaturesConfig,
+  I18nConfig,
+  OccConfig,
+  provideConfig,
+  SiteContextConfig,
+} from '@spartacus/core';
+import {
+  defaultCmsContentProviders,
+  layoutConfig,
+  mediaConfig,
+} from '@spartacus/storefront';
 
 @NgModule({
   declarations: [],
-  imports: [
+  imports: [],
+  providers: [
+    provideConfig(layoutConfig),
+    provideConfig(mediaConfig),
+    ...defaultCmsContentProviders,
+    provideConfig(<OccConfig>{
+      backend: {
+        occ: {
+          baseUrl: 'https://api.spartacus.rocks',
+        },
+      },
+    }),
+    provideConfig(<SiteContextConfig>{
+      context: {
+        urlParameters: ['baseSite', 'language', 'currency'],
+        baseSite: ['electronics-spa'],
+        language: ['en'],
+        currency: ['USD'],
+      },
+    }),
+    provideConfig(<I18nConfig>{
+      i18n: {
+        resources: translations,
+        chunks: translationChunksConfig,
+        fallbackLang: 'en',
+      },
+    }),
+    provideConfig(<FeaturesConfig>{
+      features: {
+        level: '4.1',
+      },
+    }),
   ],
-  providers: [provideConfig(layoutConfig), provideConfig(mediaConfig), ...defaultCmsContentProviders, provideConfig(<OccConfig>{
-    backend: {
-      occ: {
-        baseUrl: 'https://api.spartacus.rocks',
-      }
-    },
-  }), provideConfig(<SiteContextConfig>{
-    context: {
-      urlParameters: ['baseSite', 'language', 'currency'],
-      baseSite: ['electronics-spa'],
-      language: ['en'],
-      currency: ['USD'],
-    },
-  }), provideConfig(<I18nConfig>{
-    i18n: {
-      resources: translations,
-      chunks: translationChunksConfig,
-      fallbackLang: 'en'
-    },
-  }), provideConfig(<FeaturesConfig>{
-    features: {
-      level: '4.1'
-    }
-  })]
 })
-export class SpartacusConfigurationModule { }
+export class SpartacusConfigurationModule {}
--- src/app/spartacus/spartacus-features.module.ts
+++ src/app/spartacus/spartacus-features.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { AnonymousConsentsModule, AuthModule, CartModule, CartOccModule, CostCenterOccModule, ExternalRoutesModule, OrderOccModule, ProductModule, ProductOccModule, UserOccTransitionalModule, UserTransitionalModule } from "@spartacus/core";
 import { AddressBookModule, AnonymousConsentManagementBannerModule, AnonymousConsentsDialogModule, BannerCarouselModule, BannerModule, BreadcrumbModule, CartComponentModule, CartPageEventModule, CategoryNavigationModule, CmsParagraphModule, ConsentManagementModule, FooterNavigationModule, HamburgerMenuModule, HomePageEventModule, LinkModule, LoginRouteModule, LogoutModule, MyCouponsModule, MyInterestsModule, NavigationEventModule, NavigationModule, NotificationPreferenceModule, OrderCancellationModule, OrderDetailsModule, OrderHistoryModule, OrderReturnModule, PaymentMethodsModule, ProductCarouselModule, ProductDetailsPageModule, ProductFacetNavigationModule, ProductImagesModule, ProductIntroModule, ProductListingPageModule, ProductListModule, ProductPageEventModule, ProductReferencesModule, ProductSummaryModule, ProductTabsModule, ReplenishmentOrderDetailsModule, ReplenishmentOrderHistoryModule, ReturnRequestDetailModule, ReturnRequestListModule, SearchBoxModule, SiteContextSelectorModule, StockNotificationModule, TabParagraphContainerModule, WishListModule } from "@spartacus/storefront";
-import { UserFeatureModule } from './features/user/user-feature.module';
-import { SmartEditFeatureModule } from './features/smartedit/smart-edit-feature.module';
-import { ProductVariantsFeatureModule } from './features/product/product-variants-feature.module';
-import { CheckoutFeatureModule } from './features/checkout/checkout-feature.module';
-import { CartSavedCartFeatureModule } from './features/cart/cart-saved-cart-feature.module';
-import { CartQuickOrderFeatureModule } from './features/cart/cart-quick-order-feature.module';
 import { AsmFeatureModule } from './features/asm/asm-feature.module';
+import { CartQuickOrderFeatureModule } from './features/cart/cart-quick-order-feature.module';
+import { CartSavedCartFeatureModule } from './features/cart/cart-saved-cart-feature.module';
+import { CheckoutFeatureModule } from './features/checkout/checkout-feature.module';
+import { ProductVariantsFeatureModule } from './features/product/product-variants-feature.module';
+import { SmartEditFeatureModule } from './features/smartedit/smart-edit-feature.module';
+import { UserFeatureModule } from './features/user/user-feature.module';
+import { PersonalizationFeatureModule } from './features/tracking/personalization-feature.module';
+import { StoreFinderFeatureModule } from './features/storefinder/store-finder-feature.module';
 
 @NgModule({
   declarations: [],
@@ -90,6 +92,8 @@ import { AsmFeatureModule } from './features/asm/asm-feature.module';
     CartSavedCartFeatureModule,
     CartQuickOrderFeatureModule,
     AsmFeatureModule,
+    PersonalizationFeatureModule,
+    StoreFinderFeatureModule,
   ]
 })
 export class SpartacusFeaturesModule { }
